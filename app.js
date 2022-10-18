6f311657d56838e641f991793cf8528085076bc2
Fondi99 <matiasfondini344@gmail.com>
2022-10-18 18:48:38 -0300

    Configuraciones en el puerto

--- app.js
+++ app.js
@@ -3,12 +3,11 @@ const app = express();
 const path = require('path');
 
 app.use(express.static(path.resolve(__dirname,'./public')));
+//Levantar nuestro servidor
+const port= process.env.PORT || 3030;
+app.listen(port,()=>console.log(`Servidor corriendo en el puerto ${port}`))
 
-//Abro servidor
-app.listen(3030,()=>
-    console.log("Servidor abierto en el puerto 3030.")
-);
-
+//Pagina Principal
 app.get('/',(req,res)=>{
     res.sendFile(path.resolve(__dirname, './views/home.html'))
 });
